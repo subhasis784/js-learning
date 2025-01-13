7fae059674b8997acc72d0b7964a5c6e6f8bf7b1
Subhasis Patra <115163678+subhasis784@users.noreply.github.com>
2025-01-13 08:30:25 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "subhasis@gmail.com"
+var accountPassword = "12345"
+accountCity = "Odisha"
+
+/*accountId = 2 */ //Notallowed
+accountEmail = "subha@gmailcom"
+accountPassword = "983758"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity])
